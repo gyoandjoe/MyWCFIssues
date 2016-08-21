c8ec7097c37ccf38c58168bc88b74fdd0fe3090a
gyoandjoe <win-g@hotmail.com>
2016-08-21 16:14:21 -0500

    primer commmit

---
+++ MyWCFIssues.Repositorio/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetaSystems.Repositorio
+{
+    public class Class1
+    {
+        public void ObtenerFacturas(int noPagina, int tamanoPaginacion = 10)
+        {
+            //C#
+            string connString = "Data Source=FAMILYFRIENDLY;Integrated Security=SSPI;Initial Catalog=TEstNetaSystems;";
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT CustomerId, CompanyName FROM Customers";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@noPagina", noPagina);
+                cmd.Parameters.AddWithValue("@registrosXPagina",tamanoPaginacion );
+
+                conn.Open();
+
+                SqlParameter returnValue = new SqlParameter();
+                //returnValue.Direction
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.SelectCommand = cmd;
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+
+                {
+                    while (dr.Read())
+                        Console.WriteLine("{0}\t{1}", dr.GetString(0), dr.GetString(1));
+                }
+            }
+        }
+    }
+}
