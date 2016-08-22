dc0e40fae6be6d99a6ec1c44fe897781dbdb1ed2
Gyo <win-g@hotmail.com>
2016-08-22 05:35:44 -0500

    [Actualizacion de script SQL]

--- SQL/NetaSystemsTest.sql
+++ SQL/NetaSystemsTest.sql
@@ -3,7 +3,7 @@ create database TEstNetaSystems
 use TestNetaSystems
 go
 
-create table Facturacion(
+create table Factura(
 Id int IDENTITY(1,1) primary key,
 FechaFacturacion datetime not null,
 CantidadFacturada decimal not null,
@@ -11,15 +11,25 @@ NombrePersona varchar(30)
 )
 go
 
-create procedure sp_ObtenerFacturasXPagina
+insert into Factura(FechaFacturacion,CantidadFacturada,NombrePersona) values (GETDATE(),'92238','Ramos')
+go	
+
+alter procedure sp_ObtenerFacturasXPagina
 @noPagina int,
 @registrosXPagina int
 as
 begin
-select * from Facturacion order by FechaFacturacion ASC
+set @noPagina = @noPagina-1
+select * from Factura order by FechaFacturacion ASC
 OFFSET @noPagina * @registrosXPagina ROW
 FETCH NEXT @registrosXPagina ROWS ONLY
 end
 go
 
-exec sp_ObtenerFacturasXPagina 1, 10
+
+select * from Factura
+
+
+exec sp_ObtenerFacturasXPagina 3, 2
+
+
